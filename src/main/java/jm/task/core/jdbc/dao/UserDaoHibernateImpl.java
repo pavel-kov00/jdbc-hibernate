3921d475a8857ff8feddf48285ccf01bab3516f8
Pavel Kovylin <pavel-kov00@yandex.ru>
2023-01-28 15:47:37 +0600

    add DAO hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -5,6 +5,7 @@ import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -15,31 +16,108 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            session.createSQLQuery("create table if not exists users " +
+                    "(   id       bigint(20) not null auto_increment ," +
+                    "    name     varchar(40) null,"   +
+                    "    lastname varchar(40) null,"   +
+                    "    age      tinyint     null,"   +
+                    "    primary key(id)           "   +
+                    ") ;").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            session.createSQLQuery("drop table if exists users").executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
 
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            User user = new User(name,lastName, age);
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            User user = session.get(User.class,id);
+            session.delete(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        List<User> list = new ArrayList<>();
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            list = session.createQuery("from User").getResultList();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
+        return list;
     }
 
     @Override
     public void cleanUsersTable() {
-
+        Session session = null;
+        try {
+            session = Util.getSessionFactory().getCurrentSession();
+            session.beginTransaction();
+            session.createQuery("delete User");
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.getTransaction().rollback();
+        } finally {
+            session.close();
+        }
     }
 }
